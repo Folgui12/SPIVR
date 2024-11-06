5276f5b79ca1edab0b9ff0947a6f867bf184876c
Folgui12 <folguerasbenjamin@gmail.com>
2024-11-06 15:34:41 -0300

    FirstAPK

---
+++ SPIVR/Assets/CupDetection.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+public class CupDetection : MonoBehaviour
+{
+    [SerializeField] private float coffeTime;
+
+    private float timeToMake;
+
+    private bool startProcess;
+
+    private XRGrabInteractable cupInteractable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (startProcess)
+        {
+            timeToMake += Time.deltaTime;
+
+            if (timeToMake > coffeTime)
+            {
+                cupInteractable.enabled = true;
+            }
+        }
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.CompareTag("Cup"))
+        {
+            cupInteractable = other.gameObject.GetComponent<XRGrabInteractable>();
+            HandDetection cupOnHand = other.gameObject.GetComponent<HandDetection>();
+
+            if (!cupOnHand.OnHand)
+            {
+                cupInteractable.transform.position = transform.position;
+                cupInteractable.enabled = false;
+            }
+        }
+    }
+
+}
